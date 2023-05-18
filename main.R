f85f5bbe29c2d0f73a09a5e05ad0c7b30b096c3e
kaoru-droid <67271607+kaoru-droid@users.noreply.github.com>
2023-05-18 16:30:33 +0900

    first meeting

---
+++ main.R
@@ -0,0 +1,73 @@
+# main.R
+
+# Loading data
+dataPath = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/DS1.txt")
+# dsに変更
+ds1 = read.table(dataPath)[2]
+ds1 = as.numeric(ds1$V2)
+
+# dataPath = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/DS2.txt")
+# ds2 = read.table(dataPath)[2]
+# ds2 = as.numeric(ds2$V2)
+
+# dataPath = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/DS3.txt")
+# ds3 = read.table(dataPath)[2]
+# ds3 = as.numeric(ds3$V2)
+
+# dataPath = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/DS4.txt")
+# ds4 = read.table(dataPath)[2]
+# ds4 = as.numeric(ds4$V2)
+
+# Load Hal wavelet estimation module
+WSE_Path = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/WaveletShrinkageEestimation.R")
+print("Load Hal wavelet estimation module")
+print(WSE_Path)
+source(WSE_Path)
+
+# Calculate the cumulative value of the data
+cul_ds1 = toCulData(ds1)
+print(cul_ds1)
+
+# Calling Hal wavelet estimation based on Anscombe data transformation
+# idata_hft = HFT(ds1,"ut","s")
+# cul_idata_hft = toCulData(idata_hft)
+# mseRes = MSE(cul_idata_hft,cul_ds1)
+# print(round(mseRes,3))
+
+# idata_hft = HFT(ds1,"ut","h")
+# cul_idata_hft = toCulData(idata_hft)
+# mseRes = MSE(cul_idata_hft,cul_ds1)
+# print(round(mseRes,3))
+
+
+# Calling Hal wavelet estimation based on Fisz data transformation
+# idata_hft = HFT(ds1,"ut")
+# Calling Hal wavelet estimation based on Anscombe data transformation
+# idata_hat = HAT(ds1,"none")
+# Calling the Bartlett data transformation based Hal wavelet estimation
+# idata_hbt = HBT(ds1,"none")
+
+# Calling Hal wavelet estimation without data transformation
+idata_h = H(ds1)
+
+# Calling MSE
+mse = MSE(ds1,idata_h)
+
+#Output estimation results
+print("ds1")
+print(ds1)
+# print("HFT")
+# print(idata_hft)
+# print("HBT")
+# print(idata_hbt)
+# print("HAT")
+# print(idata_hat)
+print("H")
+print(idata_h)
+# .datで出力
+
+print("MSE")
+print(mse)
+
+
+# 小数点3桁でtxtに出力
