7fd662937341c2ebaba5db353f4ac8261f061eab
kaoru-528 <origami.kaoru@gmail.com>
2023-06-01 10:46:20 +0900

    fix

--- main.R
+++ main.R
@@ -23,8 +23,13 @@ ds = as.numeric(ds$V2)
 # Load Hal wavelet estimation module
 WSE_Path = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/WaveletShrinkageEestimation.R")
 print("Load Hal wavelet estimation module")
-print(WSE_Path)
+# print(WSE_Path)
 source(WSE_Path)
+
+WT_Path = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/WaveletTransform.R")
+print("Load Hal wavelet Transformation module")
+# print(WT_Path)
+source(WT_Path)
 
 # Calculate the cumulative value of the data
 # cul_ds1 = toCulData(ds1)
@@ -52,20 +57,32 @@ source(WSE_Path)
 # Calling Hal wavelet estimation without data transformation
 # create file name and path
 time = Sys.time() %>% format("%d-%H-%M-%S")
-file_name <- paste0(time, ".dat")
-directory_path <- "./output/NDT_WSE/"
-file_path <- paste0(directory_path, file_name)
+file_name = paste0(time, ".dat")
+directory_path = "./output/NDT_WSE/"
+file_path = paste0(directory_path, file_name)
 
 file.create(file_path)
 
-hard = round(H(ds, "ldt", "h", 5), digits = 3)
-soft = round(H(ds, "ldt", "s", 5), digits = 3)
-edata_h = data.frame(hard, soft)
-write.table(edata_h, file_path)
-hard = round(H(ds, "ldt", "h", 4), digits = 3)
-soft = round(H(ds, "ldt", "s", 4), digits = 3)
-edata_h = data.frame(edata_h, hard, soft)
-write.table(edata_h, file_path)
+for(i in 3:log(getGroupLength(length(ds)), base = 2)){
+    
+    hard = round(H(ds, "ldt", "h", i), digits = 3)
+    soft = round(H(ds, "ldt", "s", i), digits = 3)
+    if(i == 3){
+        J = sprintf("J = %d", i)
+        new_data <- c(J,J)
+        edata_h = data.frame(hard, soft)
+        edata_h <- rbind(new_data,edata_h)
+    }
+    else{
+        J = sprintf("J = %d", i)
+        new_data <- c(J,J)
+        edata_h_s = data.frame(hard, soft)
+        edata_h_s <- rbind(new_data,edata_h_s)
+        edata_h = data.frame(edata_h, edata_h_s)
+    }
+    write.table(edata_h, file_path)
+}
+
 
 
 #Output estimation results
