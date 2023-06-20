9ada1ee20fb1222b2009b058b179bb760b043fd9
kaoru-droid <67271607+kaoru-droid@users.noreply.github.com>
2023-06-20 23:59:40 +0900

    add all

--- main.R
+++ main.R
@@ -31,6 +31,16 @@ print("Load Hal wavelet Transformation module")
 # print(WT_Path)
 source(WT_Path)
 
+create_file = function(i, directory_path, time)
+{
+    file_name_edata = paste0(time,"_edata_J=",i  ,".csv")
+    file_name_coe = paste0(time,"_coe_J=",i  ,".csv")
+    edata = paste0(directory_path, file_name_edata)
+    coe = paste0(directory_path, file_name_coe)
+    file_path = list(edata = edata, coe = coe)
+    return(file_path)
+}
+
 # Calculate the cumulative value of the data
 # cul_ds1 = toCulData(ds1)
 # print(cul_ds1)
@@ -49,26 +59,132 @@ source(WT_Path)
 #Output estimation results
 # H
 # Calling Hal wavelet estimation without data transformation
-# time = Sys.time() %>% format("%H-%M-%S")
-# for(i in 2:log(getGroupLength(length(ds)), base = 2)){
-#     hard = H(ds, "ldt", "h", i)
-#     soft = H(ds, "ldt", "s", i)
-#     hard_coe= rbind("Cs",as.data.frame(t(sapply(hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(hard$Denoise_Ds, unlist))))
-#     soft_coe= rbind("Cs",as.data.frame(t(sapply(soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(soft$Denoise_Ds, unlist))))
-#     coe = rbind("hard",hard_coe,"soft",soft_coe)
-#     file_name_edata = paste0(time,"_edata_J=",i  ,".csv")
-#     file_name_coe = paste0(time,"_coeJ=",i  ,".csv")
-#     directory_path = "./output/NDT_WSE/"
-#     file_path_edata = paste0(directory_path, file_name_edata)
-#     file_path_coe = paste0(directory_path, file_name_coe)
-#     edata_h_s = list(hard = round(hard$idata, digits = 3), soft = round(soft$idata, digits = 3))
-#     write.csv(edata_h_s, file_path_edata, row.names = FALSE)
-#     write.csv(coe, file_path_coe, row.names = FALSE)
-# }
+time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/NDT_WSE/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    hard = H(ds, "ldt", "h", i)
+    soft = H(ds, "ldt", "s", i)
+    edata = list(hard = round(hard$idata, digits = 3), soft = round(soft$idata, digits = 3))
+    hard_coe= rbind("Cs",as.data.frame(t(sapply(hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(hard$Denoise_Ds, unlist))))
+    soft_coe= rbind("Cs",as.data.frame(t(sapply(soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(soft$Denoise_Ds, unlist))))
+    coe = rbind("hard",hard_coe,"soft",soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(edata, file_path$edata, row.names = FALSE)
+    write.csv(coe, file_path$coe, row.names = FALSE)
+}
 
-# HAT
+# HAT_A1
 # Calling Hal wavelet estimation without data transformation
 time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/DT_Ans_WSE/A1/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    ut_hard = HAT(ds, "ut", "h", 1,i)
+    ut_soft = HAT(ds, "ut", "s", 1,i)
+    ut_edata = list(ut_hard = round(hard$idata, digits = 3), soft = round(ut_soft$idata, digits = 3))
+    ut_hard_coe= rbind("Cs",as.data.frame(t(sapply(ut_hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_hard$Denoise_Ds, unlist))))
+    ut_soft_coe= rbind("Cs",as.data.frame(t(sapply(ut_soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_soft$Denoise_Ds, unlist))))
+    ut_coe = rbind("hard",ut_hard_coe,"soft",ut_soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(ut_edata, file_path$edata, row.names = FALSE)
+    write.csv(ut_coe, file_path$coe, row.names = FALSE)
+}
+
+# HAT_A2
+# Calling Hal wavelet estimation without data transformation
+time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/DT_Ans_WSE/A2/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    ut_hard = HA2T(ds, "ut", "h", 1,i)
+    ut_soft = HA2T(ds, "ut", "s", 1,i)
+    ut_edata = list(ut_hard = round(hard$idata, digits = 3), soft = round(ut_soft$idata, digits = 3))
+    ut_hard_coe= rbind("Cs",as.data.frame(t(sapply(ut_hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_hard$Denoise_Ds, unlist))))
+    ut_soft_coe= rbind("Cs",as.data.frame(t(sapply(ut_soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_soft$Denoise_Ds, unlist))))
+    ut_coe = rbind("hard",ut_hard_coe,"soft",ut_soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(ut_edata, file_path$edata, row.names = FALSE)
+    write.csv(ut_coe, file_path$coe, row.names = FALSE)
+}
+
+# HAT_A3
+# Calling Hal wavelet estimation without data transformation
+time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/DT_Ans_WSE/A3/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    ut_hard = HA3T(ds, "ut", "h", 1, i)
+    ut_soft = HA3T(ds, "ut", "s", 1, i)
+    ut_edata = list(ut_hard = round(hard$idata, digits = 3), soft = round(ut_soft$idata, digits = 3))
+    ut_hard_coe= rbind("Cs",as.data.frame(t(sapply(ut_hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_hard$Denoise_Ds, unlist))))
+    ut_soft_coe= rbind("Cs",as.data.frame(t(sapply(ut_soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_soft$Denoise_Ds, unlist))))
+    ut_coe = rbind("hard",ut_hard_coe,"soft",ut_soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(ut_edata, file_path$edata, row.names = FALSE)
+    write.csv(ut_coe, file_path$coe, row.names = FALSE)
+}
+
+# HBT_B1
+# Calling Hal wavelet estimation without data transformation
+time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/DT_Bar_WSE/B1/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    ut_hard = HBT(ds, "ut", "h", 1, i)
+    ut_soft = HBT(ds, "ut", "s", 1, i)
+    ut_edata = list(ut_hard = round(hard$idata, digits = 3), soft = round(ut_soft$idata, digits = 3))
+    ut_hard_coe= rbind("Cs",as.data.frame(t(sapply(ut_hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_hard$Denoise_Ds, unlist))))
+    ut_soft_coe= rbind("Cs",as.data.frame(t(sapply(ut_soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_soft$Denoise_Ds, unlist))))
+    ut_coe = rbind("hard",ut_hard_coe,"soft",ut_soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(ut_edata, file_path$edata, row.names = FALSE)
+    write.csv(ut_coe, file_path$coe, row.names = FALSE)
+}
+
+
+# HBT_B2
+# Calling Hal wavelet estimation without data transformation
+time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/DT_Bar_WSE/B2/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    ut_hard = HB2T(ds, "ut", "h", 1, i)
+    ut_soft = HB2T(ds, "ut", "s", 1, i)
+    ut_edata = list(ut_hard = round(hard$idata, digits = 3), soft = round(ut_soft$idata, digits = 3))
+    ut_hard_coe= rbind("Cs",as.data.frame(t(sapply(ut_hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_hard$Denoise_Ds, unlist))))
+    ut_soft_coe= rbind("Cs",as.data.frame(t(sapply(ut_soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_soft$Denoise_Ds, unlist))))
+    ut_coe = rbind("hard",ut_hard_coe,"soft",ut_soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(ut_edata, file_path$edata, row.names = FALSE)
+    write.csv(ut_coe, file_path$coe, row.names = FALSE)
+}
+
+# HFitT
+# Calling Hal wavelet estimation without data transformation
+time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/DT_Fit_WSE/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    ut_hard = HFitT(ds, "ut", "h", 1, i)
+    ut_soft = HFitT(ds, "ut", "s", 1, i)
+    ut_edata = list(ut_hard = round(ut_hard$idata, digits = 3), soft = round(ut_soft$idata, digits = 3))
+    ut_hard_coe= rbind("Cs",as.data.frame(t(sapply(ut_hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_hard$Denoise_Ds, unlist))))
+    ut_soft_coe= rbind("Cs",as.data.frame(t(sapply(ut_soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_soft$Denoise_Ds, unlist))))
+    ut_coe = rbind("hard",ut_hard_coe,"soft",ut_soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(ut_edata, file_path$edata, row.names = FALSE)
+    write.csv(ut_coe, file_path$coe, row.names = FALSE)
+}
+
+# HFreT
+# Calling Hal wavelet estimation without data transformation
+time = Sys.time() %>% format("%H-%M-%S")
+directory_path = "./output/DT_Fre_WSE/"
+for(i in 2:log(getGroupLength(length(ds)), base = 2)){
+    ut_hard = HFreT(ds, "ut", "h", 1, i)
+    ut_soft = HFreT(ds, "ut", "s", 1, i)
+    ut_edata = list(ut_hard = round(ut_hard$idata, digits = 3), soft = round(ut_soft$idata, digits = 3))
+    ut_hard_coe= rbind("Cs",as.data.frame(t(sapply(ut_hard$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_hard$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_hard$Denoise_Ds, unlist))))
+    ut_soft_coe= rbind("Cs",as.data.frame(t(sapply(ut_soft$Cs, unlist))),"Ds",as.data.frame(t(sapply(ut_soft$Ds, unlist))),"Denoise_Ds",as.data.frame(t(sapply(ut_soft$Denoise_Ds, unlist))))
+    ut_coe = rbind("hard",ut_hard_coe,"soft",ut_soft_coe)
+    file_path = create_file(i, directory_path, time)
+    write.csv(ut_edata, file_path$edata, row.names = FALSE)
+    write.csv(ut_coe, file_path$coe, row.names = FALSE)
+}
 
 
 
