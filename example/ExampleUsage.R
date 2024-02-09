2ed855e999adc560cdaba7bc50503c1fcf33494b
松井薫 <kaoru_m2mba@kaoru-2.local>
2024-02-09 10:16:40 +0900

    upadate

--- example/ExampleUsage.R
+++ example/ExampleUsage.R
@@ -1,15 +1,21 @@
 # When you use this program for the first time, you need to install the following packages.
 # install.packages("tidyverse")
+
 # Load libraries
 library(tidyverse)
 # Load Hal wavelet estimation module
-WSE_Path = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/src/WaveletShrinkageEestimation.R")
+WSE_Path = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/src/WaveletShrinkageEstimation.R")
 source(WSE_Path)
 
 # Load data
-ds = loadData(dataPath = "/DS/DS6.txt")
-# hard = wse(data = ds, dt = "none", thresholdName = "ldt", thresholdMode = "h", index = 3, initThresholdvalue = 1)
-# soft = wse(data = ds, dt = "none", thresholdName = "ldt", thresholdMode = "s", index = 3, initThresholdvalue = 1)
-# createResult(hard = hard, soft = soft, index = 3, resultPath = "./output/NDT_WSE/")
+ds = loadData(dataPath = "/example/ExampleDS.txt")
 
-hard = tipsh(data = ds, thresholdName = "ldt", thresholdMode = "h", index = 3)
+# Perform WSE in (none, ldt, h, 3, 1)
+hard = wse(data = ds, dt = "none", thresholdName = "ldt", thresholdMode = "h", index = 3, initThresholdvalue = 1)
+# Perform WSE in (none, ldt, s, 3, 1)
+soft = wse(data = ds, dt = "none", thresholdName = "ldt", thresholdMode = "s", index = 3, initThresholdvalue = 1)
+# Create result
+createResult(hard = hard, soft = soft, index = 3, resultPath = "./output/NDT_WSE/")
+
+# Pefrom tipshWSE in (h, 1, 3)
+tipshHard = tipsh(data = ds, thresholdMode = "h", var = 1, index = 3)
