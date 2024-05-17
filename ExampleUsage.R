bec237103321810e8b310df4c93ccae7c3bdde6d
kaoru-528 <origami.kaoru@gmail.com>
2024-05-17 13:33:13 +0900

    Fix somebugs

--- ExampleUsage.R
+++ ExampleUsage.R
@@ -9,11 +9,11 @@ source(WSE_Path)
 
 # Load data
 ds = LoadData(DataPath = "/example/ExampleDS.txt")
-Hard = Wse(Data = ds, DataTransform = "A3", ThresholdName = "lht", ThresholdMode = "h", Index = 3, InitThresholdValue = 1, Var = 1)
+Hard = Wse(Data = ds, DataTransform = "none", ThresholdName = "ldt", ThresholdMode = "h", Index = 3, InitThresholdValue = 1, Var = 1)
 # Perform WSE in (none, ldt, s, 3, 1)
-# Soft = Wse(Data = ds, DataTransform = "none", ThresholdName = "ldt", ThresholdMode = "s", Index = 3, InitThresholdValue = 1, Var = 1)
+Soft = Wse(Data = ds, DataTransform = "none", ThresholdName = "ldt", ThresholdMode = "s", Index = 3, InitThresholdValue = 1, Var = 1)
 # Create result
-# CreateResult(Hard = Hard, Soft = Soft, Index = 3, ResultPath = "./output/NDT_WSE/")
+CreateResult(Hard = Hard, Soft = Soft, Index = 3, ResultPath = "./output/NDT_WSE/")
 
 # Pefrom tipshWSE in (h, 1, 3)
-# TipshHard = Tipsh(Data = ds, ThresholdMode = "h", Var = 1, Index = 3)
+TipshHard = Tipsh(Data = ds, ThresholdMode = "h", Var = 1, Index = 3)
